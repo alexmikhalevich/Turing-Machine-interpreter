760efe614f24d06d5a9b82cf0e72d46bac06f3c3
maked0n <alex.mikhalevich@gmail.com>
2015-05-04 17:22:15 +0300

    Syntax highlightning class

--- csyntaxhighlighter.cpp
+++ csyntaxhighlighter.cpp
@@ -24,3 +24,43 @@ CSyntaxHighlighter::CSyntaxHighlighter(QTextDocument* parent) : QSyntaxHighlight
     sh_commentStartExpr = QRegExp("/\\*");
     sh_commentEndExpr = QRegExp("\\*/");
 }
+
+void CSyntaxHighlighter::highlightBlock(const QString& text)
+{
+    foreach (HighlightningRule& rule, sh_highlightningRules)
+    {
+        QRegExp expression(rule.pattern);
+        int index = expression.indexIn(text);
+        while(index >= 0)
+        {
+            int length = expression.matchedLength();
+            setFormat(index, length, rule.format);
+            index = expression.indexIn(text, index + length);
+        }
+    }
+
+    /*
+     *block states:
+     *0 - out of a comment
+     *1 - in a comment
+     */
+
+    setCurrentBlockState(0);
+    int startIndex = 0;
+    if(previousBlockState() != 1)
+        startIndex = sh_commentStartExpr.indexIn(text);
+    while(startIndex >= 0)
+    {
+        int endIndex = sh_commentEndExpr.indexIn(text);
+        int commentLength = 0;
+        if(endIndex == -1)
+        {
+            setCurrentBlockState(1);
+            commentLength = text.length() - startIndex;
+        }
+        else
+            commentLength = endIndex - startIndex;      //NOTE: + commentEndExpression.matchedLength() ???
+        setFormat(startIndex, commentLength, sh_multilineCommentFormat);
+        startIndex = sh_commentEndExpr.indexIn(text, startIndex + commentLength);
+    }
+}
