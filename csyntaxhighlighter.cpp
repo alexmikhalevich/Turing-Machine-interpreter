92d77ffabb7b278250d07daf97f08cdfd496e36b
maked0n <alex.mikhalevich@gmail.com>
2015-05-04 14:20:03 +0300

    Highlightning rules added

--- csyntaxhighlighter.cpp
+++ csyntaxhighlighter.cpp
@@ -1,5 +1,26 @@
 #include "csyntaxhighlighter.h"
 
-CSyntaxHighlighter::CSyntaxHighlighter()
+CSyntaxHighlighter::CSyntaxHighlighter(QTextDocument* parent) : QSyntaxHighlighter(parent)
 {
+    HighlightningRule rule;
+
+    sh_directionFormat.setFontWeight(QFont::Bold);
+    sh_directionFormat.setForeground(Qt::darkGray);
+    rule.pattern = QRegExp("[RLN]{1,10}");
+    rule.format = sh_directionFormat;
+    sh_highlightningRules.append(rule);
+
+    sh_stateFormat.setForeground(Qt::cyan);
+    rule.pattern = QRegExp("q*{1,5}");
+    rule.format = sh_stateFormat;
+    sh_highlightningRules.append(rule);
+
+    sh_singleLineCommentFormat.setForeground(Qt::darkGreen);
+    rule.pattern = QRegExp("//[^\n]*");
+    rule.format = sh_singleLineCommentFormat;
+    sh_highlightningRules.append(rule);
+
+    sh_multilineCommentFormat.setForeground(Qt::darkGreen);
+    sh_commentStartExpr = QRegExp("/\\*");
+    sh_commentEndExpr = QRegExp("\\*/");
 }
--- csyntaxhighlighter.h
+++ csyntaxhighlighter.h
@@ -4,8 +4,24 @@
 
 class CSyntaxHighlighter : public QSyntaxHighlighter
 {
+    Q_OBJECT
 public:
-    CSyntaxHighlighter();
+    CSyntaxHighlighter(QTextDocument* parent = 0);
+protected:
+    void highlightBlock(const QString &text);
+private:
+    struct HighlightningRule
+    {
+        QRegExp pattern;
+        QTextCharFormat format;
+    };
+    QVector<HighlightningRule> sh_highlightningRules;
+    QTextCharFormat sh_stateFormat;
+    QTextCharFormat sh_directionFormat;
+    QTextCharFormat sh_singleLineCommentFormat;
+    QTextCharFormat sh_multilineCommentFormat;
+    QRegExp sh_commentStartExpr;
+    QRegExp sh_commentEndExpr;
 };
 
 #endif // CSYNTAXHIGHLIGHTER_H
